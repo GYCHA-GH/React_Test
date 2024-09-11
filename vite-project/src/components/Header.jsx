8c541e4b22b2ae270042a3bc5bd174450e2b6aad
Zhivetin Roman <roman.zhimetin@gmail.com>
2024-09-11 22:55:51 +0200

    qqw

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,30 @@
+//Портированые картинки не работают(
+import './Header.css'
+export default function Header() {
+  return(
+    <header>
+      <nav>
+        <div className='container'>
+          <a href="https://www.figma.com/design/Cy6i1bosgIuGfDASwABzFu/IT-Support-Group-%7C-Site-(Copy)-(Copy)?node-id=372-556&node-type=frame&t=o4SJIm3z4api6S0g-0">
+          </a>
+          <ul>
+            <li><a href="">about</a></li>
+          </ul>
+          <ul>
+            <li><a href="">our services</a></li>
+          </ul>
+          <ul>
+            <li><a href="">job openings</a></li>
+          </ul>
+          <a href="">
+          </a>
+          <div className='Lbutton'>
+            <button>contact us</button>
+          </div>
+          
+        </div>
+        
+      </nav>
+    </header>
+  )
+}
