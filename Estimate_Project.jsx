da5177eb93258997bff94ea16ce7a873b60cb6b1
Живетин Роман <124517815+GYCHA-GH@users.noreply.github.com>
2024-09-23 11:01:05 +0200

    ghg

---
+++ Estimate_Project.jsx
@@ -0,0 +1,8 @@
+import './Estimate_Project.css'
+export default function Estimate_Project_button(){
+    return(
+    <div className='Estimate_Project_button_container'>
+      <button className='Estimate_Project_button'>Estimate project</button>
+    </div>
+    )
+}
